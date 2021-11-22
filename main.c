bb2d0293bd3614654315cd551a6643404eda836b
HTXuser1 <82179264+HTXuser1@users.noreply.github.com>
2021-11-22 12:52:16 +0100

    Add files via upload
    første c-program i r21

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world   eller goddag på Dansk!\n");
+    printf("Hello world   eller goddag på Dansk!\n");
+    return 0;
+}
